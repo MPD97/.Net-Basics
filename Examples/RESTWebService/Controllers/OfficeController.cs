bd7125153d16a78d767c58059531c7c4e65dbb77
MPD97 <mateuszpracadom@gmail.com>
2020-02-11 12:59:16 +0100

    OfficeController GetAll refactor and added logger

--- Examples/RESTWebService/Controllers/OfficeController.cs
+++ Examples/RESTWebService/Controllers/OfficeController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SimpleOfficeRepositoryCore.Data.Entities;
 
 namespace RESTWebService.Controllers
@@ -15,10 +17,12 @@ namespace RESTWebService.Controllers
     public class OfficeController : ControllerBase
     {
         private OfficeContext _officeContext;
+        private ILogger<OfficeContext> _logger;
 
-        public OfficeController(OfficeContext context)
+        public OfficeController(OfficeContext context, ILogger<OfficeContext> logger)
         {
             _officeContext = context;
+            _logger = logger;
         }
 
         public async Task<IActionResult> GetAll()
@@ -26,10 +30,13 @@ namespace RESTWebService.Controllers
             try
             {
                 var offices = await _officeContext.Offices.ToListAsync();
+                _logger.LogInformation($"Requested: [{MethodBase.GetCurrentMethod().ReflectedType.Name.Replace(">d__3","").Replace("<","")}], in: [{GetType().Name}] calass.");
+
                 return Ok(offices);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger.LogError(ex.ToString());
                 return StatusCode(StatusCodes.Status500InternalServerError, "Failed to load data from database");
             }
         }
