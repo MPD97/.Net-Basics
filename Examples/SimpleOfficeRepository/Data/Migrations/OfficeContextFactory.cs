af529e74463eb73be71f8e16b8cf5e17256f1539
Mateusz <mateusz@o2.pl>
2020-02-11 10:08:35 +0100

    OfficeContextFactory.cs Added

---
+++ Examples/SimpleOfficeRepository/Data/Migrations/OfficeContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using SimpleOfficeRepository.Data.Entities;
+using System;
+
+namespace SimpleOfficeRepository.Data.Migrations
+{
+    class OfficeContextFactory : IDesignTimeDbContextFactory<OfficeContext>
+    {
+        public OfficeContext CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
