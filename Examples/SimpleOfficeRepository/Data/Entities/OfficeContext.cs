a0b4c9856c760676017dd7826bbcb63ba527a9eb
Mateusz <mateusz@o2.pl>
2020-02-11 09:24:47 +0100

    Added MS.EFC reference

---
+++ Examples/SimpleOfficeRepository/Data/Entities/OfficeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace SimpleOfficeRepository.Data.Entities
+{
+    class OfficeContext : DbContext
+    {
+    }
+}
