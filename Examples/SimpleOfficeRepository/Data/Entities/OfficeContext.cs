9557d60129164271b9d847086bd90c6628d660d9
Mateusz <mateusz@o2.pl>
2020-02-11 09:27:08 +0100

    OfficeContext.cs added ctor with properties

--- Examples/SimpleOfficeRepository/Data/Entities/OfficeContext.cs
+++ Examples/SimpleOfficeRepository/Data/Entities/OfficeContext.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +11,11 @@ namespace SimpleOfficeRepository.Data.Entities
 {
     class OfficeContext : DbContext
     {
+        private readonly IConfiguration _config;
+
+        public OfficeContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            _config = configuration;
+        }
     }
 }
