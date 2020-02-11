788e7ccca30adec35f1dc13080e9918b792c7c01
Mateusz <mateusz@o2.pl>
2020-02-11 09:17:20 +0100

    Person.cs creating properties

--- Examples/SimpleOfficeRepository/Data/Entities/Person.cs
+++ Examples/SimpleOfficeRepository/Data/Entities/Person.cs
@@ -8,6 +8,17 @@ namespace SimpleOfficeRepository.Data.Entities
 {
     class Person
     {
-
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string Surename { get; set; }
+        public int Age { get; set; }
+        public Sex Sex { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+    public enum Sex
+    {
+        Other = 0,
+        Female = 1,
+        Male = 2
     }
 }
