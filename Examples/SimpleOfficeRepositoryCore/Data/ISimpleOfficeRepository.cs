a88c69481f39959c91f7fdb7f1229a064f0025e0
MPD97 <mateuszpracadom@gmail.com>
2020-02-11 15:25:42 +0100

    ISimpleOfficeRepository added

---
+++ Examples/SimpleOfficeRepositoryCore/Data/ISimpleOfficeRepository.cs
@@ -0,0 +1,32 @@
+﻿using SimpleOfficeRepositoryCore.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleOfficeRepositoryCore.Data
+{
+    interface ISimpleOfficeRepository
+    {
+        // General 
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveChangesAsync();
+
+        // Desk
+        Task<Desk[]> GetAllDesksAsync(bool includeOwner = false, bool includeRoom = false);
+        Task<Desk> GetDeskAsync(int deskId, bool includeOwner, bool includeRoom = false);
+
+        // Office
+        Task<Office[]> GetAllOfficesAsync();
+        Task<Office> GetOfficeAsync(int officeId, bool includeEmployees = false);
+
+        // Person
+        Task<Person[]> GetAllPersonsAsync();
+        Task<Person> GetPersonAsync(int personId);
+        
+        // Room
+        Task<Room[]> GetAllRoomsAsync(bool includeDesks = false);
+        Task<Room> GetRoomAsync(int roomId);
+    }
+}
