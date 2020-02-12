272ba49e396c104b1116b20afb4ae51b9cb8e9c4
MPD97 <mateuszpracadom@gmail.com>
2020-02-12 11:39:19 +0100

    Added serverside validation for OfficeModel

--- Examples/SimpleOfficeRepositoryCore/Models/OfficeModel.cs
+++ Examples/SimpleOfficeRepositoryCore/Models/OfficeModel.cs
@@ -1,6 +1,7 @@
 ﻿using SimpleOfficeRepositoryCore.Data.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace RESTWebService.ViewModels
 {
     public class OfficeModel
     {
+        [Required]
+        [StringLength(50)]
         public string CompanyName { get; set; }
+        [Required]
+        [StringLength(40)]
         public string Location { get; set; }
         public ICollection<Person> Employees { get; set; }
         public ICollection<Room> Rooms { get; set; }
